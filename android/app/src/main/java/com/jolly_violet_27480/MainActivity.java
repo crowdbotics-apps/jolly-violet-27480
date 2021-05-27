f822135f22947d2492faefa396edfa88d896e5f0
Crowdbotics <team@crowdbotics.com>
2021-05-27 16:38:40 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/jolly_violet_27480/MainActivity.java
@@ -0,0 +1,15 @@
+package com.jolly_violet_27480;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "jolly_violet_27480";
+  }
+}
